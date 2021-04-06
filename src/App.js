da1518bf94c55603fa8f3e2f379e243945a4e3b4
anhhuyvu <48982728+anhhuyvu@users.noreply.github.com>
2021-04-06 09:53:13 +0700

    Add styles

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import logo from './logo.svg';
-import './App.css';
+import './App.scss';
+
+import cssButton from './button.module.scss';
 
 function App() {
   return (
@@ -17,6 +19,7 @@ function App() {
         >
           Learn React
         </a>
+        <button className={cssButton.button}>Error</button>
       </header>
     </div>
   );
